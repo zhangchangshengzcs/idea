6940468a51cabfef03bfc966a4b3f83da0811236
zhangchangshengzcs <422540002@qq.com>
2019-04-03 10:33:16 +0800

    删除一个项目

--- json20181210/src/main/java/com/hgc/dao/CatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hgc.dao;
-
-import com.hgc.bean.Cat;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CatRepository extends CrudRepository<Cat,Integer> {
-}
